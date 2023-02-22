f3b85454bae62602ddb82aa87f2bed9fbe82d793
Egor Parfenyuk <egor7909@inbox.ru>
2023-02-22 20:59:56 +0300

    4.2 | Deleted counter component with main page

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -3,9 +3,6 @@ import React from 'react'
 import { useTranslation } from 'react-i18next'
 
 
-import { Counter } from 'entities/Counter'
-
-
 const MainPage = () => {
     const { t } = useTranslation('main')
 
@@ -13,10 +10,6 @@ const MainPage = () => {
     return (
         <div>
             {t('Главная')}
-
-            <br />
-
-            <Counter />
         </div>
     )
 }
